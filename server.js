b12fb1abaed9dfd247b381abc5e374c15a2d39c8
rob <robert_regan86@yahoo.com>
2022-12-08 09:56:15 -0500

    edit and delete plus server side rendering for homepage

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 const express = require('express')
+const multer = require('multer')
+const upload = multer()
+const sanitizeHTML = require('sanitize-html')
+const fse = require('fs-extra')
+const sharp = require('sharp')
+const path = require('path')
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
+const AnimalCard = require('./src/components/AnimalCard').default
+
+//when the app first launches make sure the upload folder exists
+fse.ensureDirSync(path.join('public', 'uploaded-photos'))
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -8,11 +21,48 @@ app.use(express.static('public'))
 
 let db
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+function passwordProtected(req, res, next) {
+  res.set('WWW-Authenticate', 'Basic realm="Animal App"')
+  console.log(req.headers.authorization)
+  if (req.headers.authorization === 'Basic YWRtaW46YWRtaW4=') {
+    next()
+  } else {
+    console.log(req.headers.authorization)
+    res.status(401).send('Authentication required')
+  }
+}
+
 app.get('/', async (req, res) => {
   const allAnimals = await db.collection('Animals').find().toArray()
-
-  res.render('home', { allAnimals })
+  const generatedHTML = ReactDOMServer.renderToString(
+    <div className='container'>
+      {!allAnimals.length && (
+        <p>There are no animals yet, the admin needs to add some :) </p>
+      )}
+      <div className='animal-grid mb-3'>
+        {allAnimals.map((animal) => (
+          <AnimalCard
+            key={animal._id}
+            name={animal.name}
+            species={animal.species}
+            photo={animal.photo}
+            id={animal._id}
+            readOnly={true}
+          />
+        ))}
+      </div>
+      <p>
+        <a href='/admin'>Login / manage the animal listings.</a>
+      </p>
+    </div>
+  )
+  res.render('home', { generatedHTML })
 })
+
+app.use(passwordProtected) // ensures all routes below are password protected
 
 app.get('/admin', (req, res) => {
   res.render('admin')
@@ -22,6 +72,91 @@ app.get('/api/animals', async (req, res) => {
   const allAnimals = await db.collection('Animals').find().toArray()
   res.json(allAnimals)
 })
+
+app.post(
+  '/create-animal',
+  upload.single('photo'),
+  cleanup,
+  async (req, res) => {
+    if (req.file) {
+      const photoFile = `${Date.now()}.jpg`
+      await sharp(req.file.buffer)
+        .resize(844, 456)
+        .jpeg({ quality: 60 })
+        .toFile(path.join('public', 'uploaded-photos', photoFile))
+      req.cleanData.photo = photoFile
+    }
+
+    console.log(req.body)
+    const info = await db.collection('Animals').insertOne(req.cleanData)
+    const newAnimal = await db
+      .collection('Animals')
+      .findOne({ _id: new ObjectId(info.insertedId) })
+
+    res.send(newAnimal)
+  }
+)
+
+app.delete('/animal/:id', async (req, res) => {
+  if (typeof req.params.id != 'string') req.params.id = ''
+  const doc = await db
+    .collection('Animals')
+    .findOne({ _id: new ObjectId(req.params.id) })
+  if (doc.photo) {
+    fse.remove(path.join('public', 'uploaded-photos', doc.photo))
+  }
+  db.collection('Animals').deleteOne({ _id: new ObjectId(req.params.id) })
+  res.send('niceee')
+})
+
+app.post(
+  '/update-animal',
+  upload.single('photo'),
+  cleanup,
+  async (req, res) => {
+    if (req.file) {
+      const photoFile = `${Date.now()}.jpg`
+      await sharp(req.file.buffer)
+        .resize(844, 456)
+        .jpeg({ quality: 60 })
+        .toFile(path.join('public', 'uploaded-photos', photoFile))
+      const info = await db
+        .collection('Animals')
+        .findOneAndUpdate(
+          { _id: new ObjectId(req.body._id) },
+          { $set: req.cleanData }
+        )
+      if (info.value.photo) {
+        fse.remove(path.join('public', 'uploaded-photos', info.value.photo))
+      }
+      res.send(photoFile)
+    } else {
+      db.collection('Animals').findOneAndUpdate(
+        { _id: new ObjectId(req.body._id) },
+        { $set: req.cleanData }
+      )
+      res.send(false)
+    }
+  }
+)
+
+function cleanup(req, res, next) {
+  if (typeof req.body.name != 'string') req.body.name = ''
+  if (typeof req.body.species != 'string') req.body.species = ''
+  if (typeof req.body._id != 'string') req.body._id = ''
+
+  req.cleanData = {
+    name: sanitizeHTML(req.body.name.trim(), {
+      allowedTags: [],
+      allowedAttributes: {},
+    }),
+    species: sanitizeHTML(req.body.species.trim(), {
+      allowedTags: [],
+      allowedAttributes: {},
+    }),
+  }
+  next()
+}
 
 async function start() {
   const client = new MongoClient(
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import axios from 'axios'
+import Axios from 'axios'
 import CreateNewForm from './components/CreateNewForm'
 import AnimalCard from './components/AnimalCard'
 
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     async function go() {
-      const response = await axios.get('/api/animals')
+      const response = await Axios.get('/api/animals')
       setAnimals(response.data)
     }
     go()
@@ -18,19 +18,19 @@ function App() {
   return (
     <div className='container'>
       <p>
-        <a href='/'>&laquo; Back to Homepage</a>
+        <a href='/'>&laquo; Back to public homepage</a>
       </p>
       <CreateNewForm setAnimals={setAnimals} />
       <div className='animal-grid'>
-        {animals.map((animal) => {
+        {animals.map(function (animal) {
           return (
             <AnimalCard
               key={animal._id}
+              name={animal.name}
+              species={animal.species}
               photo={animal.photo}
               id={animal._id}
               setAnimals={setAnimals}
-              name={animal.name}
-              species={animal.species}
             />
           )
         })}
